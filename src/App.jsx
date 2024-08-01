d159df044cc3ea614d00c2453d2672f704aa2249
Abhishek <abhi22sen2000@gmail.com>
2024-08-01 12:11:56 +0530

    done menage product

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import Routers from './Route';
 import AnimationThemeProvider from './_helper/AnimationTheme/AnimationThemeProvider';
 import CustomizerProvider from './_helper/Customizer/CustomizerProvider';
+import FilterProvider from './_helper/Ecommerce/Filter/FilterProvider';
+import ProductProvider from './_helper/Ecommerce/Product/ProductProvider';
+import CartProvider from './_helper/Ecommerce/Cart/CardProvider';
 
 const App = () => (
   <div className='App'>
     <CustomizerProvider>
       <AnimationThemeProvider>
-        <Routers />
+        <FilterProvider>
+          <CartProvider>
+            <ProductProvider>
+              <Routers />
+            </ProductProvider>
+          </CartProvider>
+        </FilterProvider>
       </AnimationThemeProvider>
     </CustomizerProvider>
   </div>
--- src/Route/Routes.jsx
+++ src/Route/Routes.jsx
@@ -1,5 +1,11 @@
 // dashbaord
 import Default  from "../Components/Dashboard/Default";
+
+// //E-commerce
+import Products from "../Components/Application/Ecommerce/Products";
+
 export const routes = [
   { path: `${process.env.PUBLIC_URL}/dashboard/default/:layout`, Component: <Default /> },
+  { path: `${process.env.PUBLIC_URL}/app/ecommerce/product/:layout`, Component: <Products /> },
+
 ];
